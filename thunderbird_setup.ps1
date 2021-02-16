c86a9ff6400e4b963c3e5fb569f539b6f3dc6801
Carsten Rossen <69820600+carsten-rossen@users.noreply.github.com>
2021-02-16 10:52:27 -0800

    Update thunderbird_setup.ps1

--- thunderbird_setup.ps1
+++ thunderbird_setup.ps1
@@ -1,10 +1,14 @@
 # Script Name: Thunderbird Setup
-# Class Name: Ops 201
 # Author Name: Carsten Rossen
-# Date of Latest Revision: 2/10/21
-# Purpose: Saves domain information to a local file.
+# Date of Latest Revision: 2/16/21
+# Purpose: Downloads and installs Mozilla Thunderbird with default settings using PowerShell. 
 
+# Main
+
+# Starts download with Microsoft Edge and waits 60 seconds for download to complete
 start microsoft-edge:https://en.softonic.com/download/mozilla-thunderbird/windows/post-download
-cd C:\Users\first\Downloads
 Start-Sleep -Seconds 60
+
+# Silently installs Thunderbird with default settings
+cd C:\Users\first\Downloads
 Start-Process '.\Thunderbird_Setup_78.7.1.exe' "/S"
