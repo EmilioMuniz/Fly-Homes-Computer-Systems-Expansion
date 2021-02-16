8ff5dbf0667c5d708c61a29fd8104fd83f7d8cbb
Carsten Rossen <69820600+carsten-rossen@users.noreply.github.com>
2021-02-16 15:15:56 -0800

    Update user_setup.ps1

--- Scripts/user_setup.ps1
+++ Scripts/user_setup.ps1
@@ -1,3 +1,13 @@
+# Script Name: User Setup
+# Author Name: Carsten Rossen
+# Date of Latest Revision: 2/16/21
+# Purpose: Creates a new local user
+
+# Main
+
+# Creates new non-administratvie user with specified password
 $Password = ConvertTo-SecureString "flyhomes123" -AsPlainText -Force
 New-LocalUser "second" -Password $Password 
 Add-LocalGroupMember -Group "Users" -Member "second"
+
+# End
