035b6276a1442e537a8af64b2ed029fcf9a4f09d
Carsten Rossen <69820600+carsten-rossen@users.noreply.github.com>
2021-02-16 14:34:39 -0800

    Update rdp_setup.ps1

--- Scripts/rdp_setup.ps1
+++ Scripts/rdp_setup.ps1
@@ -1,2 +1,11 @@
+# Script Name: Remote Desktop Setup
+# Author Name: Carsten Rossen
+# Date of Latest Revision: 2/16/21
+# Purpose: Allows an RDP connection to be established to this Windows 10 while leaving the firewall enabled.
+
+# Main
+
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -Value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+# End
