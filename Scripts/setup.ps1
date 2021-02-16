9b91c410f89ad5c24dec01025799f9a3c25de59a
Carsten Rossen <69820600+carsten-rossen@users.noreply.github.com>
2021-02-16 15:38:44 -0800

    Update setup.ps1

--- Scripts/setup.ps1
+++ Scripts/setup.ps1
@@ -5,12 +5,10 @@
 
 
 
-# Variables (ASSIGN BEFORE RUNNING)
+# Variables
 
-# Set a username for the new user using 'firstname.lastname' convention
+# Username and password for new user
 $Username = ""
-
-# Set a password for the new user using 'firstname.lastname' convention
 $Password = ""
 
 
@@ -19,14 +17,13 @@ $Password = ""
 
 # Creates new non-administratvie user with specified password
 function Setup-User {
-  $Passkey = ConvertTo-SecureString $Password -AsPlainText -Force
-  New-LocalUser $Username -Password $Passkey 
+  New-LocalUser $Username -Password $Password 
   Add-LocalGroupMember -Group "Users" -Member $Username
 }
 
 # Downloads and installs Mozilla Thunderbird with default settings using PowerShell.
 function Setup-Thunderbird {
-  cd C:\Users\username\Downloads
+  cd C:\Users\$Username\Downloads
 
   # Starts download with Microsoft Edge and waits 60 seconds for download to complete
   # Starts the download twice in case it doesn't start the first time
@@ -38,4 +35,16 @@ function Setup-Thunderbird {
   # Silently installs Thunderbird with default settings
   Start-Process '.\Thunderbird_Setup_78.7.1.exe' "/S"
 }
+
+# Allows an RDP connection to be established to this Windows 10 while leaving the firewall enabled.
+function Setup-RDP {
+  Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -Value 0
+  Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+}
+
+
+
+# Main
+
+$Password = Read-Host -AsSecureString
 
