e94f2ad019f75f29c8c52dc25e46a0f31dbd5cb7
Carsten Rossen <69820600+carsten-rossen@users.noreply.github.com>
2021-02-16 16:22:42 -0800

    Update setup.ps1

--- Scripts/setup.ps1
+++ Scripts/setup.ps1
@@ -20,9 +20,6 @@ function Setup-Thunderbird {
   cd C:\Users\$Username\Downloads
 
   # Starts download with Microsoft Edge and waits 60 seconds for download to complete
-  # Starts the download twice in case it doesn't start the first time
-  start microsoft-edge:https://en.softonic.com/download/mozilla-thunderbird/windows/post-download
-  Start-Sleep -Seconds 1
   start microsoft-edge:https://en.softonic.com/download/mozilla-thunderbird/windows/post-download
   Start-Sleep -Seconds 60
 
