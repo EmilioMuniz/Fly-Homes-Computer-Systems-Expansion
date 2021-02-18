9c9ca217360f39de0c7d25e682f1aa4e6bec1a6b
Carsten Rossen <69820600+carsten-rossen@users.noreply.github.com>
2021-02-17 17:16:14 -0800

    Update rdp_setup.ps1

--- Scripts/rdp_setup.ps1
+++ Scripts/rdp_setup.ps1
@@ -2,6 +2,7 @@
 # Author Name: Carsten Rossen
 # Date of Latest Revision: 2/16/21
 # Purpose: Allows an RDP connection to be established to this Windows 10 while leaving the firewall enabled.
+# NOTE: If running scripts is disabled on system, type 'Set-ExecutionPolicy -ExecutionPolicy Unrestricted'
 
 # Main
 
